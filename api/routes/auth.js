fc450b1dfa651fd598322ad963a3de3eebc69a45
costingh <cgheorghe0702@stud.acs.upb.ro>
2021-06-25 21:19:48 +0300

    Added login route

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -5,7 +5,6 @@ const bcrypt = require('bcrypt');
 // REGISTER USER
 router.post('/register', async (req, res) => {
     try{
-
         // encrypt password
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(req.body.password, salt);
@@ -17,12 +16,26 @@ router.post('/register', async (req, res) => {
             password: hashedPassword,
         });
 
-
         // save user and send response
         const user = await newUser.save();
         res.status(200).json(user);
     } catch (err) {
-        console.log(err)
+        res.status(500).json(error);
+    }
+});
+
+// LOGIN
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({email: req.body.email});
+        !user && res.status(404).json("user not found");
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        !validPassword && res.status(404).json("wrong password");
+
+        res.status(200).json(user);
+    } catch(err) {
+        res.status(500).json(error);
     }
 })
 
