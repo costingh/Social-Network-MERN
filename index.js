27fd27a99992fe5d5cbfbbc5d98619424db98f22
costingh <cgheorghe0702@stud.acs.upb.ro>
2021-06-25 20:33:38 +0300

    created node.js server

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
+
+dotenv.config();
+
+const bodyParser = require("body-parser")
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGO_URL, 
+    {useNewUrlParser: true, useUnifiedTopology: true}, 
+    () => {
+    console.log('connected to Mongo')
+    }
+)
+
+// middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'));
+
+app.use('/api/user', userRoute);
+app.use('/api/auth', authRoute);
+
+app.listen(8800, () => {
+    console.log('Backend server is running');
+})
