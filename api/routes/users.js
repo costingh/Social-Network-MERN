fc4d9cd4bce3c8e012760900faee8cd96d098540
costingh <cgheorghe0702@stud.acs.upb.ro>
2021-06-26 15:05:30 +0300

    following a user

--- api/routes/users.js
+++ api/routes/users.js
@@ -51,7 +51,26 @@ router.get('/:id', async (req, res) => {
 })
 
 // follow a user
+router.put('/:id/follow', async (req, res) => {
+    if(req.body.userId !== req.params.id) {
+        try {
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
 
+            if(!user.followers.includes(req.body.userId)) {
+                await user.updateOne({$push:{followers: req.body.userId}});
+                await currentUser.updateOne({$push:{followings: req.params.id}});
+                res.status(200).json('user has been followed');
+            } else {
+                res.status(403).json('You already follow this user');
+            }
+        } catch(err) {
+            res.status(500).json(err)
+        }
+    } else {
+        res.status(403).json('You can\'t follow yourself');
+    }
+})
 
 // unfollow a user
 router.get('/', (req, res) => {
