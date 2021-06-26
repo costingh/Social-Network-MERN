3547615889c664f813e43d379a5dccb2de6dd29b
costingh <cgheorghe0702@stud.acs.upb.ro>
2021-06-26 14:51:54 +0300

    Added delete account route

--- api/routes/users.js
+++ api/routes/users.js
@@ -26,7 +26,18 @@ router.put("/:id", async (req, res) => {
 })
 
 // delete user
-
+router.delete("/:id", async (req, res) => {
+    if(req.body.userId === req.params.id || req.body.isAdmin) {
+        try {
+            const user = await User.findByIdAndDelete(req.params.id);
+            res.status(200).json('Account has been deleted successfully')
+        } catch(err) {
+            return res.status(500).json(err);
+        }
+    } else {
+        return res.status(403).json('You can delete only your account!')
+    }
+})
 
 // get a user
 
