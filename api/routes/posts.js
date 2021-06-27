7a0464d85f424c439f0e1825e75867ee777e3b0b
costingh <cgheorghe0702@stud.acs.upb.ro>
2021-06-27 22:55:05 +0300

    add update post

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -12,7 +12,22 @@ router.post('/', async(req, res) =>{
         res.status(500).json(err);
     }
 })
+
 //update a post
+router.put('/:id', async(req, res) =>{
+    try {
+        const post = await Post.findById(req.params.id);
+        if(post.userId === req.body.userId) {
+            await post.updateOne({$set:req.body});
+            res.status(200).json('The post has been updated');
+        } else {
+            res.status(403).json('You can only update your post!');
+        }
+    } catch(err) {
+        res.status(500).json(err);
+    }
+
+})
 
 // delete a post
 
