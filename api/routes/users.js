5055f232450bcb819e92f2d01da8153545a439f5
costingh <cgheorghe0702@stud.acs.upb.ro>
2021-06-27 12:58:18 +0300

    Added user unfollow

--- api/routes/users.js
+++ api/routes/users.js
@@ -73,8 +73,25 @@ router.put('/:id/follow', async (req, res) => {
 })
 
 // unfollow a user
-router.get('/', (req, res) => {
-    res.send('Welcome to homepage');
+router.put('/:id/unfollow', async (req, res) => {
+    if(req.body.userId !== req.params.id) {
+        try {
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+
+            if(!user.followers.includes(req.body.userId)) {
+                await user.updateOne({$pull:{followers: req.body.userId}});
+                await currentUser.updateOne({$pull:{followings: req.params.id}});
+                res.status(200).json('user has been unfollowed');
+            } else {
+                res.status(403).json('You dont follow this user');
+            }
+        } catch(err) {
+            res.status(500).json(err)
+        }
+    } else {
+        res.status(403).json('You can\'t unfollow yourself');
+    }
 })
 
 module.exports = router;
