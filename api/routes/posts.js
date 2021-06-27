2fc2433dc5b948c43477247dc84e1a14d3b4d7d4
costingh <cgheorghe0702@stud.acs.upb.ro>
2021-06-27 23:59:06 +0300

    added get all posts

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Post = require('../models/Post');
+const User = require('../models/User');
 
 // create a post
 router.post('/', async(req, res) =>{
@@ -72,5 +73,20 @@ router.get('/:id', async(req, res) =>{
 })
 
 // get all posts of a user
+router.get('/timeline/all', async(req, res) =>{
+    try {
+        const currentUser = await User.findById(req.body.userId);
+        const userPosts = await Post.find({ userId: currentUser._id });
+        const friendPosts = await Promise.all(
+            currentUser.followings.map((friendId) => {
+                return Post.find({ userId: friendId });
+            })
+        );
+
+        res.json(userPosts.concat(...friendPosts));
+    } catch(err) {
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;
