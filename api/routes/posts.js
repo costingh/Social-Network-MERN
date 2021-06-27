19f68b4aec182176f959beed442dc0c6545e0686
costingh <cgheorghe0702@stud.acs.upb.ro>
2021-06-27 23:06:36 +0300

    add like/dislike

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -44,6 +44,22 @@ router.delete('/:id', async(req, res) =>{
 })
 
 // like a post
+router.put('/:id/like', async(req, res) =>{
+    try {
+        // Like a post
+        const post = await Post.findById(req.params.id);
+        if(!post.likes.includes(req.body.userId)) {
+            await post.updateOne({$push: { likes: req.body.userId } });
+            res.status(200).json('The post has been liked');
+        } else {
+            // Dislike a post
+            await post.updateOne({$pull: { likes: req.body.userId } });
+            res.status(403).json('Post disliked!');
+        }
+    } catch(err) {
+        res.status(500).json(err);
+    }
+})
 
 // get a post
 
