d83b322779352a9f42e6c641ec17aa561236162c
costingh <cgheorghe0702@stud.acs.upb.ro>
2021-06-28 18:52:38 +0300

    rightbar component

--- client/src/components/rightbar/Rightbar.js
+++ client/src/components/rightbar/Rightbar.js
@@ -3,7 +3,25 @@ import './rightbar.css'
 function Rightbar() {
     return (
         <div className="rightbar">
-            rightbar
+            <div className="rightbarWrapper">
+                <div className="birthdayContainer">
+                    <img src="assets/gift.png" alt="" className="birthdayImg"/>
+                    <span className="birthdayText">
+                        <b>Andrew Huighmann</b> and <b>2 other friends </b>have a birthday today
+                    </span>
+                </div>
+                <img src="assets/ad.png" alt="" className="rightbarAd"/>
+                <h4 className="rightbarTitle">Online Friends</h4>
+                <ul className="rightbarFriendList">
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="assets/person/1.jpg" alt="" className="rightbarProfileImg"/>
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Andrew</span>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
