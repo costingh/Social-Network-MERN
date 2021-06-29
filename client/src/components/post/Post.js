43fcc24e6d5ca56e551baf40f33aa80400be334a
costingh <cgheorghe0702@stud.acs.upb.ro>
2021-06-29 22:34:02 +0300

    added like/unlike functionality on dummyData

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,11 +1,16 @@
+import React, {useState} from 'react'
 import './post.css';
 import {MoreVert} from '@material-ui/icons';
 import { Users } from '../../dummyData';
 
 function Post({post}) {
-    
-    
+    const [like, setLike] = useState(post.like);
+    const [isLiked, setIsLiked] = useState(false);
 
+    const likeHandler = () => {
+        setLike(isLiked ? like - 1 : like + 1);
+        setIsLiked(!isLiked);
+      };
 
     return (
         <div className="post">
@@ -29,9 +34,9 @@ function Post({post}) {
                 </div>
                 <div className="postBottom">
                     <div className="postBottomLeft">
-                        <img src="/assets/like.png" alt="" className="likeIcon"/>
-                        <img src="/assets/heart.png" alt="" className="likeIcon"/>
-                        <span className="postLikeCounter">{post.like} people like it</span>
+                        <img src="/assets/like.png" alt="" className="likeIcon" onClick={likeHandler} />
+                        <img src="/assets/heart.png" alt="" className="likeIcon" onClick={likeHandler} />
+                        <span className="postLikeCounter">{like} people like it</span>
                     </div>
                     <div className="postBottomRight">
                         <div className="postCommentText">{post.comment } comments</div>
