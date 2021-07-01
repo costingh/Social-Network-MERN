8ec9e375d4d6c2006d0c20109e97136041154c3c
costingh <cgheorghe0702@stud.acs.upb.ro>
2021-07-01 20:31:20 +0300

    retrieve image dinamically inside topbar

--- client/src/components/topbar/Topbar.js
+++ client/src/components/topbar/Topbar.js
@@ -1,8 +1,13 @@
 import './Topbar.css'
 import { Search, Person, Chat, Notifications } from '@material-ui/icons';
 import { Link } from 'react-router-dom';
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 function Topbar() {
+    const { user } = useContext(AuthContext);
+    const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+
     return (
         <div className="topbarContainer">
             <div className="topbarLeft">
@@ -35,7 +40,17 @@ function Topbar() {
                         <span className="topbarIconBadge">1</span>
                     </div>
                 </div>
-                <img src="/assets/person/1.jpeg" alt="" className="topbarImg"/>
+                <Link to={`/profile/${user.username}`}>
+                    <img
+                        src={
+                        user.profilePicture
+                            ? PF + user.profilePicture
+                            : PF + "person/noAvatar.png"
+                        }
+                        alt=""
+                        className="topbarImg"
+                    />
+                </Link>
             </div>
         </div>
     )
