bde24733ac21ffac34b31f68e774f6cd4a484dcd
costingh <cgheorghe0702@stud.acs.upb.ro>
2021-07-01 19:51:38 +0300

    add circular progress to login/sign up buttons

--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -1,6 +1,7 @@
 import { useContext, useRef } from 'react';
 import {loginCall} from '../../apiCalls';
 import { AuthContext } from '../../context/AuthContext';
+import { CircularProgress } from "@material-ui/core";
 
 import "./login.css";
 
@@ -43,12 +44,20 @@ function Login() {
                 minLength="6"
                 className="loginInput"
               />
-              <button className="loginButton" type="submit">
-                {isFetching ? "Loading..." : "Log In"}
+              <button className="loginButton" type="submit" disabled={isFetching}>
+                {isFetching ? (
+                  <CircularProgress color="primary" size="20px" />
+                ) : (
+                  "Log In"
+                )}
               </button>
               <span className="loginForgot">Forgot Password?</span>
               <button className="loginRegisterButton">
-              Create a New Account
+                {isFetching ? (
+                  <CircularProgress color="primary" size="20px" />
+                ) : (
+                  "Create a New Account"
+                )}
               </button>
             </form>
           </div>
