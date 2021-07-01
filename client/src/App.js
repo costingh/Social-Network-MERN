4cbce36f5f22c506e1069c2146ef15a3a2546f55
costingh <cgheorghe0702@stud.acs.upb.ro>
2021-07-01 20:22:38 +0300

    handled register

--- client/src/App.js
+++ client/src/App.js
@@ -8,20 +8,20 @@ import {
   Route,
   Redirect,
 } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
-
+  const { user } = useContext(AuthContext);
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-        <Home /> 
+          {user ? <Home /> : <Register />}
         </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
+        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
         <Route path="/register">
-        <Register />
+          {user ? <Redirect to="/" /> : <Register />}
         </Route>
         <Route path="/profile/:username">
           <Profile />
--- client/src/pages/register/Register.js
+++ client/src/pages/register/Register.js
@@ -1,41 +1,73 @@
+import axios from "axios";
+import { useRef } from "react";
+import { useHistory } from "react-router";
+
 import "./register.css";
 
 function Register() {
+  const username = useRef();
+  const email = useRef();
+  const password = useRef();
+  const passwordAgain = useRef();
+  const history = useHistory();
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    if (passwordAgain.current.value !== password.current.value) {
+      passwordAgain.current.setCustomValidity("Passwords don't match!");
+    } else {
+      const user = {
+        username: username.current.value,
+        email: email.current.value,
+        password: password.current.value,
+      };
+      try {
+        await axios.post("/auth/register", user);
+        history.push("/login");
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
     return (
         <div className="login">
         <div className="loginWrapper">
           <div className="loginLeft">
             <h3 className="loginLogo">Social</h3>
             <span className="loginDesc">
-              Connect with friends and the world around you on Lamasocial.
+              Connect with people
             </span>
           </div>
           <div className="loginRight">
-            <form className="loginBox">
-            <input
+            <form className="loginBox" onSubmit={handleClick}>
+              <input
                 placeholder="Username"
-                type="email"
                 required
+                ref={username}
                 className="loginInput"
               />
               <input
                 placeholder="Email"
-                type="email"
                 required
+                ref={email}
                 className="loginInput"
+                type="email"
               />
               <input
                 placeholder="Password"
-                type="email"
                 required
+                ref={password}
                 className="loginInput"
+                type="password"
+                minLength="6"
               />
               <input
                 placeholder="Password Again"
-                type="password"
                 required
-                minLength="6"
+                ref={passwordAgain}
                 className="loginInput"
+                type="password"
               />
               <button className="loginButton" type="submit">
                 Sign up
