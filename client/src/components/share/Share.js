5efdea5758dddc392d62829a647951e0f46b488c
costingh <cgheorghe0702@stud.acs.upb.ro>
2021-07-03 19:38:46 +0300

    show image when uploading it

--- client/src/components/share/Share.js
+++ client/src/components/share/Share.js
@@ -1,4 +1,4 @@
-import { PermMedia, Label, Room, EmojiEmotions } from '@material-ui/icons';
+import { PermMedia, Label, Room, EmojiEmotions, Cancel } from '@material-ui/icons';
 import { useContext, useRef, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import axios from "axios";
@@ -58,6 +58,12 @@ function Share() {
                     />
                 </div>
                 <hr className="shareHr"/>
+                {file && (
+                    <div className="shareImgContainer">
+                        <img className="shareImg" src={URL.createObjectURL(file)} alt="" />
+                        <Cancel className="shareCancelImg" onClick={() => setFile(null)} />
+                    </div>
+                )}
                 <form className="shareBottom" onSubmit={submitHandler}>
                     <div className="shareOptions">
                         <label htmlFor="file" className="shareOption">
