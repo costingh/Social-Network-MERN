2ec0c6dcd77f292a186a498c549c7eea32614516
costingh <cgheorghe0702@stud.acs.upb.ro>
2021-07-05 14:44:17 +0300

    initiate chat when choosing an online user

--- api/routes/conversations.js
+++ api/routes/conversations.js
@@ -42,4 +42,16 @@ router.get("/find/:firstUserId/:secondUserId", async (req, res) => {
   }
 });
 
+// get a conversation using two users id
+router.get("/find/:firstUserId/:secondUserId", async (req, res) => {
+  try {
+    const conversation = await Conversation.findOne({
+      members: { $all: [req.params.firstUserId, req.params.secondUserId] },
+    });
+    res.status(200).json(conversation)
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
